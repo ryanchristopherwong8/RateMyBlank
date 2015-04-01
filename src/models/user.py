d92ee717da4219abe2aa72fa083b66465a021185
Ryan Wong <ryancwong8@gmail.com>
2015-03-31 23:56:32 -0700

    changing publish_date to change_date

--- src/models/user.py
+++ src/models/user.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     name = models.CharField(max_length=200)
-    publish_date = models.DateField(max_length=200, auto_now_add=True)
+    created_date = models.DateField(max_length=200, auto_now_add=True)
