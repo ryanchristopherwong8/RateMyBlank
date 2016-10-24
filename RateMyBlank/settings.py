84100cc07576ad5c5c7c01b09e0a3ebb7f6d58f0
Ryan Wong <ryancwong8@gmail.com>
2016-10-23 17:54:03 -0700

    Fixes typo for heroku deployment

--- RateMyBlank/settings.py
+++ RateMyBlank/settings.py
@@ -85,7 +85,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, ‘static’),
+    os.path.join(BASE_DIR, 'static'),
 )
 #STATIC_ROOT = '../RateMyBlank/src/static'
 
