6ea366110ab4d38b766af054cc4d12553ab064f3
Ryan Wong <ryancwong8@gmail.com>
2016-10-23 17:52:54 -0700

    Correct Typo for heroku

--- RateMyBlank/settings.py
+++ RateMyBlank/settings.py
@@ -83,7 +83,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # import os
 # BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-STATIC_ROOT = ‘staticfiles’
+STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, ‘static’),
 )
